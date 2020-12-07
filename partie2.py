229723b3ab2b7178ab795077b677d3b5858005fa
Chaddaï Fouché <chaddai.fouche@gmail.com>
2020-12-07 16:55:06 +0100

    Réalisation de la partie 2

---
+++ partie2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from eval1 import sélectionner, mélanger
+
+# ouverture du fichier
+fsource = open('quizz-diu-eil.txt','r')
+
+# lecture des lignes dans une liste de
+# p-uplets (question (str), réponse (str), points (int))
+parPoints = dict()
+for ligne in fsource:
+    try:
+        q, r, p = ligne.strip().split(';')
+        if p not in parPoints:
+            parPoints[p] = []
+        parPoints[p].append((q,r,int(p)))
+    except:
+        pass
+
+fsource.close()
+
+questions = []
+for p in parPoints.keys():
+    # Sélection du nombre de questions souhaité
+    nb_questions = int(input(f'Combien de questions à {p} points souhaitez-vous ? '))
+    assert nb_questions <= len(parPoints[p]), "Trop de questions demandées, seules "+str(len(parPoints[p]))+" sont disponibles."
+    # Sélection aléatoire des questions
+    questions.extend(sélectionner(nb_questions, parPoints[p]))
+# mélange final pour éviter les successions de questions par points
+mélanger(questions)
+
+
+score = 0
+for (question, réponse, points) in questions:
+    print(question)
+    rep = input("Votre réponse : ")
+    if rep == réponse:
+        score += points
+
+# calcul du score total possible par somme des points de toutes les questions
+total = sum(p for _,_,p in questions)
+print("Vous avez obtenu", score, "points sur un total possible de", total)
+
+
+
